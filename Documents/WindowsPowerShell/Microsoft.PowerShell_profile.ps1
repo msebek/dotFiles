26993b4c0aa84d6058e9a74f476c7db1e15d3047
Matt Sebek <sebek.matt@gmail.com>
2015-10-28 07:37:51 -0700

    Adds the rest of powershell.

---
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+﻿
+# Load posh-git example profile
+. 'C:\Users\Matt\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
+
+# Alias gvim to vim
+set-alias vim gvim
+
+function up1 {
+    cd ..;
+}
+function up2 {
+    cd ..\..;
+}
+function up3 {
+    cd ..\..\..;
+}
+function up4 {
+    cd ..\..\..\..;
+}
