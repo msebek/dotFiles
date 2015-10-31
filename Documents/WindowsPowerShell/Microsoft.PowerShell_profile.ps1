94bd45519a18f3608f23a8615766f03af4899b48
Matt Sebek <msebek@andrew.cmu.edu>
2015-10-30 18:07:28 -0700

    Adds vim to PS profile

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,8 @@
 . 'C:\Users\Matt\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
 
 # Alias gvim to vim
-set-alias vim gvim
+set-alias vim "C:\Program Files (x86)\Vim\vim74\gvim.exe"
+set-alias gvim vim
 
 function up1 {
     cd ..;
